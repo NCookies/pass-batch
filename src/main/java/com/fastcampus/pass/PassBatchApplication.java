31a18127a7ae5ae11e7af4d113822684e677400f
SeungWoo Ryu <swstar21c@gmail.com>
2024-11-13 14:35:52 +0900

    #2 - Spring Batch 5 버전에 맞게 간단 예제 구현
    - Spring Batch 5 버전에서 여러 변경점이 생겼는데 강의 코드는 이 이전 버전을 사용 중이다.
      - 때문에 deprecated 된 코드들을 현재 버전에 맞게 수정해야 한다.
    - 여러 사이트들을 참고하여 간단한 batch 예제 코드를 작성해보았다.

--- src/main/java/com/fastcampus/pass/PassBatchApplication.java
+++ src/main/java/com/fastcampus/pass/PassBatchApplication.java
@@ -1,10 +1,46 @@
 package com.fastcampus.pass;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
+import javax.sql.DataSource;
+
+@RequiredArgsConstructor
 @SpringBootApplication
 public class PassBatchApplication {
+
+	private final DataSource batchDataSource;
+
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(batchDataSource);
+	}
+
+	@Bean
+	public Step passStep(JobRepository jobRepository) {
+		return new StepBuilder("passStep", jobRepository)
+				.tasklet((contribution, chunkContext) -> {
+					System.out.println("Execute PassStep");
+					return RepeatStatus.FINISHED;
+				}, transactionManager())
+				.build();
+	}
+
+	@Bean
+	public Job passJob(JobRepository jobRepository) {
+		return new JobBuilder("passJob", jobRepository)
+				.start(passStep(jobRepository))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassBatchApplication.class, args);
