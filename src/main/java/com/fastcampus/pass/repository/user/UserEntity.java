096b1a2bdda6b2f47924899a84efb59ca6f2a246
SeungWoo Ryu <swstar21c@gmail.com>
2024-11-27 12:07:34 +0900

    Revert "[batch] 수업 종료 후 이용권 차감 구현"

--- src/main/java/com/fastcampus/pass/repository/user/UserEntity.java
+++ src/main/java/com/fastcampus/pass/repository/user/UserEntity.java
@@ -1,14 +1,10 @@
 package com.fastcampus.pass.repository.user;
 
 import com.fastcampus.pass.repository.BaseEntity;
-import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Type;
-
-import java.util.Map;
 
 @Getter
 @Setter
@@ -23,17 +19,6 @@ public class UserEntity extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private UserStatus status;
     private String phone;
-
-    // json 형태로 저장되어 있는 문자열 데이터를 Map으로 매핑한다.
-    @Type(JsonType.class)
-    private Map<String, Object> meta;
-
-    public String getUuid() {
-        String uuid = null;
-        if (meta.containsKey("uuid")) {
-            uuid = String.valueOf(meta.get("uuid"));
-        }
-        return uuid;
-    }
+    private String meta;
 
 }
